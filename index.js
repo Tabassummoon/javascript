6b58d38771a3eca951b4d9ab5b10675c58f8971f
unknown <tabassummoon8@gmail.com>
2024-12-12 15:35:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// variable declar and value assign
+// data type
+// string data type
+const colors = "red";
+const App_Name = "instagram";
+const Greeting = "Hello";
+const userName = "Tabassum";
+const bloodGroup = "o+";
+let message = "Good morning";
+let car = "Toyota";
+
+// number data type
+let age = 20;
+let temperature = 26;
+let score = 80;
+
+// boolean data type
+const isAdmin = false;
+// undefined data type
+let city;
+// object data type
+let city = null;
+// symbol data type
+let sym = Symbol('name')
+
+
+
+console.log(typeof(city));
---
+++ operators.js
@@ -0,0 +1,22 @@
+// 1. arithmatich operator ---> +, -, *, /, %,++, --, **
+// 2. assignment operator ----> = , +=, -+, *=, /=, %=, **=
+// 3. comparesion operator ---> ==, ===, !=, !==, >, <, <=,>=
+// 4. logical operator ----> &&, ||,!
+// 5. tarnery   operator ---> ?
+// let firstNumber = 500;
+// let secondNumber = 8;
+// let remainder = firstNumber % secondNumber
+// let quotient = firstNumber / secondNumber
+
+// let firstProduct = 3000;
+// let secondProduct = 4000;
+// let thirdProduct = 5000;
+// total price 10% discount.
+
+// let totalPrice = (3000+4000+5000)
+// console.log(taka)
+// taka--;
+// console.log(taka)
+// for(let i = 0; i <= 500; i++)
+let friendAge = [ 23, 34, 23, 34, 56, 78, 12, 15, 67 ];
+console.log(friendAge)
